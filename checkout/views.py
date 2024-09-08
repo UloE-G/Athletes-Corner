0320084d4023f065e1708c63728df5ebacc69f19
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-09-08 17:03:59 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
@@ -15,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PuXFFJKKE7zswA9yNUDpemvkCz5wTv2Rfn27H5BFYg7IPZntletz9Zx82Mo5UBssoTHrdeWQQ01d33S4eQBa7GH00jiVDXpFZ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
