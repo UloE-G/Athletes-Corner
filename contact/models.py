30f1c11e96f9e28a95d17f10d6ee15b035a3c2a4
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-09-16 19:15:38 +0000

    Removed blank from reasons field

--- contact/models.py
+++ contact/models.py
@@ -9,13 +9,13 @@ class Return(models.Model):
         ('Order did not fit', 'Order did not fit'),
         ('Order arrived too late', 'Order arrived too late'),
         ('Order was not as described', 'Order was not as described'),
-        ('Other (If other please specify down below)', 'Other (If other please specify down below)'),
+        ('Other (Please specify down below)', 'Other (Please specify down below)'),
     )
     reasons = models.CharField(max_length=254, choices=reason, 
-                               blank=True, null=True,
+                               null=True,
                                default='Wrong color')
     other = models.TextField(blank=True, 
-                             null=True)
+                            null=True)
 
     def __str__(self):
         return self.order_number
