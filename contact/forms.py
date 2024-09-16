6620c1bef4c69950dc4855b8cc1d9b9d45c7757f
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-09-16 09:46:24 +0000

    Created return form class

---
+++ contact/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Return
+
+class ReturnForm(forms.ModelForm):
+    class Meta:
+        model = Return
+        fields = ('order_number', 'reasons', 'other')
