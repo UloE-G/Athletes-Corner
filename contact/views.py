639cf1f3741c0392bfbf52d29234bf2f021a34c3
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-09-16 09:45:14 +0000

    Added post request to contact view

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .models import Return
+from .forms import ReturnForm
 
 # Create your views here.
 def contact(request):
     """ A view to return the contact page """
 
-    template = 'contact/contact.html'
+    if request.method == "POST":
+        print("Recived post request")
+        return_form = ReturnForm(data=request.POST)
+        if return_form.is_valid():
+            print("Valid request")
+            return_form.save()
+            messages.success(request, "Message received! We will contact you in the coming days.")
+        else:
+            messages.error(request, 'Message failed, please look over form')
 
-    return render(request, template)
+    return_form = ReturnForm()
+    returns = Return
+
+    template = 'contact/contact.html'
+    context  = {
+        'returns': returns,
+        "return_form": return_form
+    }
+
+    return render(request, template, context)
