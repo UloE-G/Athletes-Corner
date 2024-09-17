63b4dce1b403e66df3ed2998349ae3ce1b066615
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-09-17 17:22:28 +0000

    Added email form in footer

--- home/views.py
+++ home/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .models import Newsletter
+from .forms import NewsletterForm
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    if request.method == "POST":
+        print("Recived post request")
+        letter_form = NewsletterForm(data=request.POST)
+        if letter_form.is_valid():
+            print("Valid request")
+            letter_form.save()
+            messages.success(request, 
+                             "Thank you :) Please check email for an exclusive welcome discount!!!")
+        else:
+            messages.error(request, 
+                           'Request failed, please make sure email was typed in correctly')
+    
+    letter_form = NewsletterForm()
+    letter = Newsletter
+
+    context = {
+        'letter': letter,
+        'letter_form': letter_form
+    }
+
+    return render(request, 'home/index.html', context)
