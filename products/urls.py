0827ed7f8d87d22a723dcd622081c2a9ef6caf7c
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-09-18 15:33:03 +0000

    Created edit and delete urls

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('edit_review/<int:review_id>/', views.edit_review, name='edit_review'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('delete_review/<int:review_id>/', views.delete_review, name='delete_review'),
 ]
