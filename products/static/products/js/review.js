fdfea8ee24d8890f937d1fc72c8e7060681b4239
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-09-18 15:31:56 +0000

    Created javascript to edit and delete reviews

--- products/static/products/js/review.js
+++ products/static/products/js/review.js
@@ -5,17 +5,22 @@ const reviewTitle = document.getElementById("id_title");
 const reviewForm = document.getElementById("reviewForm");
 const submitButton = document.getElementById("submitButton");
 
+
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
 console.log("IN STATIC JS")
 
 /**
 * Initializes edit functionality for the provided edit buttons.
 * 
 * For each button in the `editButtons` collection:
-* - Retrieves the associated comment's ID upon click.
+* - Retrieves the associated review's ID upon click.
 * - Fetches the content of the corresponding comment.
-* - Populates the `commentText` input/textarea with the comment's content for editing.
+* - Populates the `reviewRating`, `reviewText`, and `reviewTitle` input/textarea with the comment's content for editing.
 * - Updates the submit button's text to "Update".
-* - Sets the form's action attribute to the `edit_comment/{reviewId}` endpoint.
+* - Sets the form's action attribute to the `review_edit/{reviewId}` endpoint.
 */
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
@@ -27,6 +32,24 @@ for (let button of editButtons) {
     reviewRating.value = rating; 
     reviewText.value = reviewContent;
     submitButton.innerText = "Update";
-    reviewForm.setAttribute("action", `edit_review/${reviewId}`);
+    reviewForm.setAttribute("action", `/products/edit_review/${reviewId}/`);
+  });
+}
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific comment.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
+for (let button of deleteButtons) {
+  button.addEventListener("click", (e) => {
+    let reviewId = e.target.getAttribute("data-review_id");
+    deleteConfirm.href = `/products/delete_review/${reviewId}/`;
+    deleteModal.show();
   });
 }
