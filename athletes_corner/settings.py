9fb0700523fad2e1a57e48eab43428db6fdb3a5d
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-09-28 11:15:51 +0000

    Set debug to true

--- athletes_corner/settings.py
+++ athletes_corner/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-uloeg-athletescorner-01sxzp7l6rm.ws.codeinstitute-ide.net',
                  'athletes-corneru.herokuapp.com']
