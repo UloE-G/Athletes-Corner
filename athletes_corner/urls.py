adf06cdddc455d7227b5f44eaf94008bbab84ec4
Ulo Eruma-Gborie <uloerumagborie@gmail.com>
2024-10-14 18:19:57 +0000

    changed handler404 name in urls, added extra whitespace in views, and changed debug to development in settings

--- athletes_corner/urls.py
+++ athletes_corner/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'boutique_ado.views.handler404'
+handler404 = 'athletes_corner.views.handler404'
